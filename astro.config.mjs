174df2977363c5290f499858b0d2e3abd531d950
Nicolas BONNET <nicolas.bonnet@cirm-math.fr>
2023-07-04 00:18:20 +0200

    Write good root to Header.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://nicolasbonnet.github.io',
-	base: '/',
+	base: '/nicolasbonnet',
 	integrations: [mdx(), sitemap()],
 });
