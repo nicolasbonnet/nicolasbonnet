6f4cdee54a524ea049f32a4a45f6ea7f03646075
Nicolas BONNET <nicolas.bonnet@cirm-math.fr>
2023-07-03 22:52:09 +0200

    Fix config astro to github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://nicolasbonnet.github.io',
-	base: '/nicolasbonnet',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 });
